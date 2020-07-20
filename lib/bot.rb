c89fa51ee802f7f6027a7b876f44f828baeb4522
unknown <nusretbutunay@gmail.com>
2020-07-20 16:10:11 +0300

    add weather and picsum commands

--- lib/bot.rb
+++ lib/bot.rb
@@ -13,6 +13,21 @@ module InspireBot
         title 'time'
         desc 'Current Local Time'
       end
+
+      command :date do
+        title 'date'
+        desc 'Current Date'
+      end
+
+      command :picsum do
+        title 'Random picture generator'
+        desc 'Generates a 200x300 picture'
+      end
+
+      command :weather do
+        title 'weather <cityname>'
+        desc 'Current Weather data for specified city '
+      end
     end
   end
 end
---
+++ lib/helpers/weather.rb
@@ -0,0 +1,17 @@
+require 'net/http'
+require 'json'
+
+module InspireBot
+  attr_reader :forecast
+  class Weather
+    API_KEY = '1fdc8f2066cefcc799fcd789b3199c56'.freeze
+
+    def current_weather(city)
+      @url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=1fdc8f2066cefcc799fcd789b3199c56"
+      @uri = URI(@url)
+      @response = Net::HTTP.get(@uri)
+      forecast = JSON.parse(@response)
+      "The weather in #{city}:  #{forecast['weather'][0]['description']} \n The temperature in #{city}:  #{((forecast['main']['temp']).to_i-273.15).truncate(2)} °C"
+    end
+  end
+end
